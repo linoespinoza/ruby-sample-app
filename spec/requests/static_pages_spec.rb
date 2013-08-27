4335a0e22448b9f1d1021e957e4a350b9e249698
Lino Espinoza <woztheman@gmail.com>
2013-08-26 20:45:10 -0500

    Añadiendo nuevas rutas y vistas

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,17 +6,17 @@ describe "Static pages" do
 
   describe "Home Page" do
     it "should have the h1 'Sample App'" do
-      visit '/static_pages/home'
+      before { visit root_path }
       page.should have_selector('h1', :text => 'Sample App')
     end
 
     it "should have the right title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should have_selector('title', :text => "#{base_title}")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       page.should_not have_selector('title', :text => "| Home")
     end
   end
@@ -45,9 +45,15 @@ describe "Static pages" do
       page.should have_selector('title', 
         :text => "#{base_title} | About Us")
     end
-  end
+  end 
 
   describe "Contact Us Page" do
+    
+    it "should have the h1 'Contact' " do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', text: 'Contact')
+    end
+
     it "should have the right title" do
       visit '/static_pages/contact'
       page.should have_selector('title',
